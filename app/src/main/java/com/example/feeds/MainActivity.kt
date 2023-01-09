92525ba46538b90ef81b289345be8052947b687e
the-anant-kumar <anantkr0202@gmail.com>
2023-01-10 01:10:48 +0530

    added share button

--- app/src/main/java/com/example/feeds/MainActivity.kt
+++ app/src/main/java/com/example/feeds/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.feeds
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,10 +18,12 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_news.*
 
 class MainActivity : AppCompatActivity(), NewItemClicked {
 
     private lateinit var mAdapter: NewsListAdapter
+    private lateinit var mNewsArray: ArrayList<News>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +39,7 @@ class MainActivity : AppCompatActivity(), NewItemClicked {
         val jsonObjectRequest = object : JsonObjectRequest(Request.Method.GET, url, null,
             {
                 val newsJsonArray = it.getJSONArray("articles")
-                val newsArray = ArrayList<News>()
+                val mNewsArray = ArrayList<News>()
                 for(i in 0 until newsJsonArray.length()){
                     val newsJsonObject = newsJsonArray.getJSONObject(i)
                     val news = News(
@@ -42,9 +50,9 @@ class MainActivity : AppCompatActivity(), NewItemClicked {
                         newsJsonObject.getString("urlToImage"),
                         newsJsonObject.getJSONObject("source")
                     )
-                    newsArray.add(news)
+                    mNewsArray.add(news)
                 }
-                mAdapter.updateNews(newsArray)
+                mAdapter.updateNews(mNewsArray)
             },
             {
                 Log.d("Error: ", it.toString())
@@ -60,9 +68,27 @@ class MainActivity : AppCompatActivity(), NewItemClicked {
             MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
+
     override fun onItemClicked(item: News) {
         val builder =  CustomTabsIntent.Builder()
         val customTabsIntent = builder.build()
         customTabsIntent.launchUrl(this, Uri.parse(item.url))
     }
+
+    override fun onShareButtonClicked(item: News, imageView: ImageView) {
+        val url = item.url
+        val bitmapDrawable = imageView!!.drawable as BitmapDrawable
+        val bitmap = bitmapDrawable.bitmap
+        val path = MediaStore.Images.Media.insertImage(contentResolver, bitmap,"tempimage", null)
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/*"
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_TEXT, item.title+"\n"+url)
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path))
+
+        val chooser = Intent.createChooser(intent, "Share News")
+        startActivity(chooser)
+    }
+
 }
--- app/src/main/java/com/example/feeds/NewsListAdapter.kt
+++ app/src/main/java/com/example/feeds/NewsListAdapter.kt
@@ -1,12 +1,21 @@
 package com.example.feeds
 
+import android.content.ContentResolver
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.media.Image
+import android.net.Uri
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.file.FileResource
 import kotlinx.android.synthetic.main.item_news.view.*
 
 class NewsListAdapter(private val listener: NewItemClicked) : RecyclerView.Adapter<NewsViewHolder> (){
@@ -19,6 +28,10 @@ class NewsListAdapter(private val listener: NewItemClicked) : RecyclerView.Adapt
         view.setOnClickListener{
             listener.onItemClicked(items[viewHolder.adapterPosition])
         }
+        viewHolder.share.setOnClickListener{
+            listener.onShareButtonClicked(items[viewHolder.adapterPosition], viewHolder.image)
+        }
+
         return viewHolder
     }
 
@@ -28,6 +41,7 @@ class NewsListAdapter(private val listener: NewItemClicked) : RecyclerView.Adapt
         holder.content.text = currentItem.content
         holder.source.text = currentItem.source["name"].toString()
         Glide.with(holder.itemView.context).load(currentItem.imageUrl).into(holder.image)
+
     }
 
     override fun getItemCount(): Int {
@@ -47,9 +61,10 @@ class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val image: ImageView = itemView.image
     val source: TextView = itemView.source
     val content: TextView = itemView.content
+    val share: Button = itemView.share
 }
 
 interface NewItemClicked{
     fun onItemClicked(item : News)
-
+    fun onShareButtonClicked(item: News, imageView: ImageView)
 }
